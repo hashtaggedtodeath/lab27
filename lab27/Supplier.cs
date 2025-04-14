ced85b4b1c9ccf5963797d9f54d03b01cd90aecb
hashtaggedtodeath <145613017+hashtaggedtodeath@users.noreply.github.com>
2025-04-14 11:28:02 +0300

    v2

--- lab27/Supplier.cs
+++ lab27/Supplier.cs
@@ -4,7 +4,7 @@ public class Supplier
 {
     public required int SupplierID { get; set; }
     public required string Name { get; set; }
-    public required string Address { get; set; }
+    public required string Adress { get; set; }
     public required string Phone { get; set; }
 }
 }
